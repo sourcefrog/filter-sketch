66b556e83aa662b37045100988c586cc2cb0ee57
Martin Pool <mbp@sourcefrog.net>
2021-11-04 05:42:37 -0700

    try taking &mut to filters

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
+// trait Filter = FnMut(i32) -> bool;
+
 struct Opt<'f> {
-    filter: &'f mut dyn FnMut(i32) -> bool,
+    filter: Option<&'f mut dyn FnMut(i32) -> bool>,
 }
 
 impl<'f> Opt<'f> {
     fn new() -> Opt<'static> {
-        fn always_true(_i: i32) -> bool {
-            true
-        }
+        Opt { filter: None }
+    }
+
+    fn filter<'g>(self, filter: &'g mut dyn FnMut(i32) -> bool) -> Opt<'g> {
         Opt {
-            filter: &mut always_true,
+            filter: Some(filter),
         }
     }
 
-    fn run(self) {
+    fn run(&mut self) {
         for i in 0..10 {
-            if (self.filter)(i) {
-                println!("{}", i);
+            if let Some(ref mut filter) = self.filter {
+                if filter(i) {
+                    println!("{}", i);
+                }
             }
         }
     }
 }
 
 fn main() {
+    println!("no filter");
     Opt::new().run();
+
+    println!("inner fn filter");
+    fn is_odd(i: i32) -> bool {
+        i % 2 == 1
+    }
+    // Requirer a "&mut" which is not ideal, but it works.
+    Opt::new().filter(&mut is_odd).run();
+
+    println!("stateless closure");
+    Opt::new().filter(&mut |i| i % 3 == 0).run();
 }
