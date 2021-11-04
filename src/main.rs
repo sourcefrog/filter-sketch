cdfc1cf4c9565a1e08548b5b67112123739d88e4
Martin Pool <mbp@sourcefrog.net>
2021-11-04 06:28:18 -0700

    Pass closures by value
    Store in a box dyn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 /*! An experiment in accumulating callback closures into a builder pattern.
+ *
+ * If the filters are passed in as `&mut` or `&` then rustc understands how they can constrain
+ * the lifetime of the build [Opt] and so they can reference and mutate local variables.
+ *
+ * Alternatively they can be passed by value but also with a lifetime constraint.
+ * The functions that receive them by value are parameterized by the incoming
+ * Fn type, but they can box it (to cope with whatever size the closure is) and
+ * include a dyn trait pointer. This seems pretty nice.
  */
 
 use std::fmt::Write;
 
 struct Opt<'f> {
-    filter: Option<&'f mut dyn FnMut(i32) -> bool>,
-    printer: Option<&'f mut dyn FnMut(i32)>,
+    filter: Option<Box<dyn FnMut(i32) -> bool + 'f>>,
+    printer: Option<Box<dyn FnMut(i32) + 'f>>,
 }
 
 impl<'f> Opt<'f> {
@@ -16,17 +24,23 @@ impl<'f> Opt<'f> {
         }
     }
 
-    fn filter(self, filter: &'f mut dyn FnMut(i32) -> bool) -> Opt<'f> {
+    fn filter<F>(self, filter: F) -> Opt<'f>
+    where
+        F: FnMut(i32) -> bool + 'f,
+    {
         Opt {
-            filter: Some(filter),
+            filter: Some(Box::new(filter)),
             printer: self.printer,
         }
     }
 
-    fn printer(self, printer: &'f mut dyn FnMut(i32)) -> Opt<'f> {
+    fn printer<P>(self, printer: P) -> Opt<'f>
+    where
+        P: FnMut(i32) + 'f,
+    {
         Opt {
             filter: self.filter,
-            printer: Some(printer),
+            printer: Some(Box::new(printer)),
         }
     }
 
@@ -54,19 +68,19 @@ fn main() {
         i % 2 == 1
     }
     // Requirer a "&mut" which is not ideal, but it works.
-    Opt::new().filter(&mut is_odd).run();
+    Opt::new().filter(is_odd).run();
 
     println!("stateless closure");
-    Opt::new().filter(&mut |i| i % 3 == 0).run();
+    Opt::new().filter(|i| i % 3 == 0).run();
 
     println!("read local variable");
     let primes = vec![2, 3, 5, 7];
-    Opt::new().filter(&mut |i| primes.contains(&i)).run();
+    Opt::new().filter(|i| primes.contains(&i)).run();
 
     println!("mutate local variable");
     let mut seen: Vec<i32> = Vec::new();
     Opt::new()
-        .filter(&mut |i| {
+        .filter(|i| {
             seen.push(i);
             true
         })
@@ -75,15 +89,15 @@ fn main() {
 
     println!("with two closures");
     Opt::new()
-        .filter(&mut |i| i & 2 == 0)
-        .printer(&mut |i| println!("{:x}", i))
+        .filter(|i| i & 2 == 0)
+        .printer(|i| println!("{:x}", i))
         .run();
 
     println!("accumulate results");
     let mut output = String::new();
     Opt::new()
-        .filter(&mut |i| i & 2 == 0)
-        .printer(&mut |i| write!(&mut output, "{:#x}, ", i).unwrap())
+        .filter(|i| i & 2 == 0)
+        .printer(|i| write!(&mut output, "{:#x}, ", i).unwrap())
         .run();
     assert_eq!(output, "0x0, 0x1, 0x4, 0x5, 0x8, 0x9, ");
 }
